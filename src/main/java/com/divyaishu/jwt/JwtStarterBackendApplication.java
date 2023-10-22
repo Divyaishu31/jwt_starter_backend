6881c417aa06ce0da1c6a3161d8b007a215a4c0b
Divya Agrawal <agarwaldivya3101@gmail.com>
2023-10-22 11:53:31 +0530

    Initial commit

---
+++ src/main/java/com/divyaishu/jwt/JwtStarterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.divyaishu.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtStarterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtStarterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/divyaishu/jwt/JwtStarterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyaishu.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtStarterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
